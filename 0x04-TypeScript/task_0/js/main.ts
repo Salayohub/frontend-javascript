65458c779854d54e7a5e37827ed88f2c05e918cd
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-10 15:22:59 +0100

    main.ts updated

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,48 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "Adamu",
+  lastName: "Dangote",
+  age: 20,
+  location: "Abuja"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Adeyanju",
+  age: 22,
+  location: "lagos"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+const headerRow = document.createElement("tr");
+
+const headers = ["First Name", "Location"];
+headers.forEach(headerText => {
+  const header = document.createElement("th");
+  header.textContent = headerText;
+  headerRow.appendChild(header);
+});
+
+table.appendChild(headerRow);
+
+studentsList.forEach(student => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+
+  table.appendChild(row);
+});
