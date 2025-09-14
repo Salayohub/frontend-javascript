50f8632067a8df4bd362c7e39d8a27cee4b4bd76
salahudeen abdulkareem <salayo4real@gmail.com>
2025-09-14 22:17:45 +0100

    updated

--- 0x04-TypeScript/task_4/js/subjects/Teacher.ts
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -1,6 +1,11 @@
+// <reference path="./Teacher.ts" />
+
 namespace Subjects {
-  export interface Teacher {
-    firstName: string;
-    lastName: string;
+  export class Subjects {
+    teacher: Teacher;
+
+    setTeacher(teacher:Teacher) {
+      this.teacher = teacher;
+    }
   }
 }
